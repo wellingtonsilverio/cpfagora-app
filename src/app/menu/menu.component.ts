96019b9049e47675277821bcee09df822c4e715c
Wellington Silverio <wellington.silverio@synvia.com>
2019-04-23 11:00:19 -0300

    <fix> menu effect

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,9 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  @ViewChild('mainNav') mainNav: { nativeElement: { className: string; }; };
 
-  constructor() { }
+  className = 'navbar navbar-expand-lg navbar-dark fixed-top';
 
+  constructor(public el: ElementRef) { }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+
+    if (scrollPosition > componentPosition) {
+      if (this.mainNav.nativeElement.className === this.className) {
+        this.mainNav.nativeElement.className = this.className + ' navbar-shrink';
+      }
+    } else {
+      this.mainNav.nativeElement.className = this.className;
+    }
+
+  }
   ngOnInit() {
   }
 
